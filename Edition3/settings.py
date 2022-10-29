c18df28fd58645bc255c4bee7cde4b086a8f4ed6
onyangodonomondi <onyangodonomondi@gmail.com>
2022-10-29 17:23:08 +0300

    middleware updates

--- Edition3/settings.py
+++ Edition3/settings.py
@@ -45,18 +45,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
+    
     "corsheaders.middleware.CorsMiddleware",
 
     "whitenoise.middleware.WhiteNoiseMiddleware",
+
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
-]
 
 ROOT_URLCONF = 'Edition3.urls'
 
