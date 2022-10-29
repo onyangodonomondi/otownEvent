2c9a6339c2de54ed8f0ca10c454f7610be478645
onyangodonomondi <onyangodonomondi@gmail.com>
2022-10-29 17:19:51 +0300

    updates

--- Edition3/settings.py
+++ Edition3/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
