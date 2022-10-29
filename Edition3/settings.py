aea131ddfeb85adf9e20c2943b03e1ffc1307c25
onyangodonomondi <onyangodonomondi@gmail.com>
2022-10-29 16:02:01 +0300

    db

--- Edition3/settings.py
+++ Edition3/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'Edition3.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LQqtmYGDLxBd2BiMdx7x',
+        'HOST': 'containers-us-west-80.railway.app',
+        'PORT': '5589',
     }
 }
 
